07496adfa2258d6596c7bb8bdbf2eb4ceda6ad88
Piyush Gambhir <90608533+Piyush-Gambhir@users.noreply.github.com>
2023-12-17 03:18:08 +0530

    Added Solutions

--- Challenges/PrepTrain 6 Companies - 15 Days DSA Challenge/3Sum.java
+++ Challenges/PrepTrain 6 Companies - 15 Days DSA Challenge/3Sum.java
@@ -2,10 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-
     public static ArrayList<ArrayList<Integer>> findTriplets(int[] arr, int n, int K) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
 
-        // Write your code here.
+        for (int i = 0; i < n; i++) {
+            int target = K - arr[i];
+            int front = i + 1;
+            int back = n - 1;
 
+            while (front < back) {
+                int sum = arr[front] + arr[back];
+
+                if (sum < target) {
+                    front++;
+                } else if (sum > target) {
+                    back--;
+                } else {
+                    int x = arr[front];
+                    int y = arr[back];
+
+                    ArrayList<Integer> list = new ArrayList<>();
+
+                    list.add(arr[i]);
+                    list.add(arr[front]);
+                    list.add(arr[back]);
+                    ans.add(list);
+
+                    while (front < back && arr[front] == x) {
+                        front++;
+                    }
+
+                    while (front < back && arr[back] == y) {
+                        back--;
+                    }
+                }
+            }
+
+            while (i + 1 < n && arr[i] == arr[i + 1]) {
+                i++;
+            }
+        }
+        return ans;
     }
 }
